e7195cc94edf5f1ad99e44ae4b2f56a68ebeae19
Wanlin-Lu <luwanlin@126.com>
2020-02-18 22:53:22 +0800

    post get

--- src/index.js
+++ src/index.js
@@ -9,4 +9,11 @@ app.get('/users', function(req, res) {
 	res.send([])
 })
 
+app.post('/users', function(req, res) {
+	res.send({id:0})
+})
+
+app.get('/users/:id', function(req, res) {
+	res.send({id:0})
+})
 var server = app.listen(3000)
